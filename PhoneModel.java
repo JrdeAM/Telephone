075436383af903e57f8d965cec2ec6d284d3c74c
JrdeAM <jade199528@gmail].com>
2026-09-10 22:46:13 -0300

    implementacao de notificacao de novo dígito para observadores

--- PhoneModel.java
+++ PhoneModel.java
@@ -1,17 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import interfaces.PhoneObserver;
 
 /**
  * Store a phone number, digit-by-digit
  */
 public class PhoneModel {
     private List<Integer> digits = new ArrayList<>();
+    private List<PhoneObserver> observers = new ArrayList<>();
 
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        notifyObservers();
     }
 
     public List<Integer> getDigits() {
         return digits;
+    }
+
+    public void addObserver(PhoneObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObservers() {
+        for (PhoneObserver observer : observers) {
+            observer.update();
+        }
     }
 }
--- Screen.java
+++ Screen.java
@@ -1,12 +1,19 @@
+import interfaces.PhoneObserver;
+
 /**
  * Prints things out to the screen, when needed
  * Printing to the screen:
- *  System.out.println("hello");
+ * System.out.println("hello");
  */
-public class Screen {
+public class Screen implements PhoneObserver {
     private final PhoneModel model;
 
     public Screen(PhoneModel model) {
         this.model = model;
+    }
+
+    @Override
+    public void update() {
+        System.out.println("Número atual: " + model.getDigits());
     }
 }
